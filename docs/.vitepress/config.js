2a8e87339bac795b33c27a3de113d2db57a13fea
ZeKJ <kennethjacobsen@hotmail.com>
2023-03-02 10:04:41 +0100

    Opstart af portal

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,31 @@
+export default {
+  title: "Portal",
+  description: "Just playing around.",
+  srcDir: "./",
+  base: "/Portal/",
+  //cleanUrls: true,
+  ignoreDeadLinks: true,
+  themeConfig: {
+    sidebar: [
+      {
+        text: "FMDatahub",
+      },
+      {
+        text: "Datadictionary",
+        link: "https://fmdatahub.github.io/Datadictionary",
+      },
+      {
+        text: "Onboarding",
+        link: "/Onboarding/",
+      },
+      {
+        text: "Guides",
+        link: "/Guides/",
+      },
+      {
+        text: "Documentation",
+        link: "/Documentation/",
+      },
+    ],
+  },
+};
