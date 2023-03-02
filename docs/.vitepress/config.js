c413c4a85c80557fadc83d73cccd8bfb86d9ef94
ZeKJ <kennethjacobsen@hotmail.com>
2023-03-02 10:32:18 +0100

    Ideer

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -9,22 +9,35 @@ export default {
     sidebar: [
       {
         text: "FMDatahub",
+        items: [
+          {
+            text: "Datahub",
+            link: "https://vna-dev.azurewebsites.net/",
+          },
+          {
+            text: "Datadictionary",
+            link: "https://fmdatahub.github.io/Datadictionary",
+          },
+          {
+            text: "Onboarding",
+            link: "/Onboarding/",
+          },
+          {
+            text: "Guides",
+            link: "/Guides/",
+          },
+          {
+            text: "Documentation",
+            link: "/Documentation/",
+          },
+        ],
       },
       {
-        text: "Datadictionary",
-        link: "https://fmdatahub.github.io/Datadictionary",
-      },
-      {
-        text: "Onboarding",
-        link: "/Onboarding/",
-      },
-      {
-        text: "Guides",
-        link: "/Guides/",
-      },
-      {
-        text: "Documentation",
-        link: "/Documentation/",
+        text: "Fagsystemer",
+        items: [
+          {text: "Timesafe", link: "https://www.timesafe.dk/"},
+          {text: "Dalux", link: "https://dalux.com/"},
+        ],
       },
     ],
   },
